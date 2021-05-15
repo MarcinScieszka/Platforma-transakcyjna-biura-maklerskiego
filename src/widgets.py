0f5ed4b49ab016ac054d58592a96d615a71a1ef9
Marcin Scieszka <marcin.scieszka@student.pk.edu.pl>
2021-05-15 19:04:34 +0200

    Fix pozycji elementów GUI

--- src/widgets.py
+++ src/widgets.py
@@ -11,17 +11,17 @@ class Widgets:
 
         # tworzenie etykiet
         main_title_label = Label(self.window, text=TEXT_MAIN_TITLE, bg=COLOUR_BACKGROUND, fg=COLOUR_TEXT,
-                           font=(FONT_TYPEFACE, FONT_SIZE_TITLE, TITLE_FONT_WEIGHT), pady=10)
+                           font=(FONT_TYPEFACE, FONT_SIZE_TITLE, TITLE_FONT_WEIGHT), pady=20)
         main_description_label = Label(self.window, text=TEXT_MAIN_DESCRIPTION, bg=COLOUR_BACKGROUND, fg=COLOUR_TEXT,
-                                 font=(FONT_TYPEFACE, FONT_SIZE_REGULAR), pady=10)
+                                 font=(FONT_TYPEFACE, FONT_SIZE_DESCRIPTION), pady=20)
 
         amount_label = Label(self.window, text=TEXT_AMOUNT, bg=COLOUR_BACKGROUND, fg=COLOUR_TEXT,
-                             font=(FONT_TYPEFACE, FONT_SIZE_REGULAR), pady=10)
+                             font=(FONT_TYPEFACE, FONT_SIZE_REGULAR))
 
         # wyświetlanie etykiet
-        main_title_label.grid(row=0, column=1, sticky=N)
-        main_description_label.grid(row=1, column=1, sticky=N)
-        amount_label.grid(row=3, column=0, sticky=E)
+        main_title_label.grid(row=0, column=3, sticky=N)
+        main_description_label.grid(row=1, column=3, sticky=N)
+        amount_label.grid(row=3, column=0, sticky=W)
 
     def setUpTextboxes(self):
         """Metoda tworzy oraz wyświetla pola tekstowe"""
@@ -34,7 +34,7 @@ class Widgets:
         """Metoda tworzy oraz wyświetla przyciski"""
 
         close_button = Button(self.window, text=TEXT_CLOSE_BUTTON, command=self.quit, padx=10)
-        close_button.grid(row=10, column=0)
+        close_button.grid(row=10, column=5)
 
     def quit(self):
         """Metoda zamyka główne okno aplikacji."""

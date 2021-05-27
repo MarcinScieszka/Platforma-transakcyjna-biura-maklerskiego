9814f76dfb0ffdf0dd562c1e5698cea41544b72b
Marcin Scieszka <marcin.scieszka@student.pk.edu.pl>
2021-05-27 22:28:37 +0200

    Restrukturyzacja: implementacja nowej klasy Account, która m. in. obsługuje stan wolnych środków na koncie.

--- src/platform.py
+++ src/platform.py
@@ -21,16 +21,42 @@ class Auxiliary:
 
         label_text_var.set(label_text)
 
+    @classmethod
+    def clear_entry_text(cls, entry):
+        """Metoda czyści zawartość pola tekstowego"""
+
+        entry.delete(0, END)
+
+
+class Account:
+    account_balance = 0  # aktualny stan wolnych środków na konice
+
+    def get_current_account_balance_text(self):
+        return Constants.TEXT_CURRENT_BALANCE + str(self.get_account_balance()) + Constants.TEXT_CURRENCY
+
+    def get_account_balance(self):
+        return Account.account_balance
+
+    def set_account_balance(self, amount):
+        Account.account_balance = amount
+
+    def increase_account_balance(self, amount):
+        Account.account_balance += amount
+        self.set_account_balance(round(self.get_account_balance(), 2))
+
+    def decrease_account_balance(self, amount):
+        Account.account_balance -= amount
+        self.set_account_balance(round(self.get_account_balance(), 2))
+
 
 class Platform:
     """Główna klasa platformy transakcyjnej"""
-
-    account_balance = 0  # aktualny stan wolnych środków na konice
 
     def __init__(self, window):
         self.window = window
         CreateGui.create_gui_params(window)  # wywołanie klasy ustawiającej parametry gui
         Widgets(window)
+        Account()
         Market(window)
         # TODO: hovering over buttons changes their colour
 
@@ -42,19 +68,30 @@ class Market:
         self.available_companies = DataProvider.get_companies()
         self.window = window
 
-        # stworzenie i wyświetlenie listy dostępnych firm
-        self.companies_listbox = Listbox(self.window)
-        self.companies_listbox.place(x=300, y=300)
         # TODO: add scrollbar to listbox
+        # TODO: ability to deselect company from a list or show popup-like thing
+        # TODO: handle error - clicking button when no item in listbox is selected
+
+        # stworzenie i wyświetlenie listy dostępnych firm
+        self.companies_listbox = Listbox(self.window,
+                                         bg=Constants.COLOUR_BACKGROUND,
+                                         selectbackground=Constants.COLOUR_BACKGROUND,
+                                         fg='white',
+                                         width=40,
+                                         font=Constants.FONT_TYPEFACE,
+                                         cursor='hand2',
+                                         bd=0,
+                                         highlightthickness=0)
+
+        self.companies_listbox.place(x=150, y=250)
 
         self.insert_available_companies()
 
-        # TODO: handle error - clicking button when no item in listbox is selected
         self.buy_shares_button = Button(self.window,
-                                        background='#464646',
-                                        fg='White',
+                                        background='#f1f1f1',
+                                        fg='black',
                                         bd=0,
-                                        relief=RAISED,
+                                        # relief=RAISED, # relief can be flat, groove, raised, ridge, solid, or sunken
                                         cursor="hand2",
                                         text="Zakup akcje",
                                         command=lambda: self.select_company())
@@ -64,8 +101,8 @@ class Market:
     def insert_available_companies(self):
         offset = 0
         for company in self.available_companies:
-            self.companies_listbox.insert(END, company.get_name() + company.get_symbol() + company.get_price())
-            # TODO: replace string with get_company_description_text
+            combined_name = company.get_symbol() + company.get_price()
+            self.companies_listbox.insert(END, combined_name)
 
             offset += 30
 
@@ -76,7 +113,6 @@ class Market:
         index = functools.reduce(lambda a: a, selection_tuple)  # zamiana typu tuple na int
         company = self.available_companies[index]
         print(company.get_name())
-        # TODO: ability to deselect company from a list
 
     def buy_stock(self, company):
         """Metoda obsługuje zakup akcji"""
@@ -85,7 +121,7 @@ class Market:
         pass
 
 
-class Widgets:
+class Widgets(Account):
     """Klasa obsługująca widżety"""
 
     count = 0  # zmienna zlicza ilość wywołań klasy, dzięki czemu możemy tworzyć widżety tylko podczas pierwszego wywołania klasy
@@ -94,70 +130,68 @@ class Widgets:
         self.window = window
 
         if Widgets.count == 0:  # zapewniamy jednokrotne tworzenie widżetów
-            self.create_widgets()  # tworzenie widżetów
+
+            # tworzenie etykiet
+            self.main_title_label = Label(self.window,
+                                          text=Constants.TEXT_MAIN_TITLE,
+                                          bg=Constants.COLOUR_BACKGROUND,
+                                          fg=Constants.COLOUR_TEXT,
+                                          font=(
+                                              Constants.FONT_TYPEFACE,
+                                              Constants.FONT_SIZE_TITLE,
+                                              Constants.FONT_WEIGHT_TITLE),
+                                          pady=20)
+            self.main_description_label = Label(self.window,
+                                                text=Constants.TEXT_MAIN_DESCRIPTION,
+                                                bg=Constants.COLOUR_BACKGROUND,
+                                                fg=Constants.COLOUR_TEXT,
+                                                font=(Constants.FONT_TYPEFACE,
+                                                      Constants.FONT_SIZE_DESCRIPTION),
+                                                pady=20)
+            self.amount_label = Label(self.window,
+                                      text=Constants.TEXT_AMOUNT,
+                                      bg=Constants.COLOUR_BACKGROUND,
+                                      fg=Constants.COLOUR_TEXT,
+                                      padx=20,
+                                      font=(Constants.FONT_TYPEFACE,
+                                            Constants.FONT_SIZE_REGULAR))
+            self.account_balance_label_text = StringVar()
+            self.account_balance_label_text.set(self.get_current_account_balance_text())
+            self.account_balance_label = Label(self.window,
+                                               textvariable=self.account_balance_label_text,
+                                               padx=20,
+                                               bg=Constants.COLOUR_BACKGROUND,
+                                               fg=Constants.COLOUR_TEXT,
+                                               font=(Constants.FONT_TYPEFACE,
+                                                     Constants.FONT_SIZE_REGULAR))
+
+            # tworzenie pól tekstowych
+            self.amount_text = StringVar()
+            self.amount_entry = Entry(self.window, textvariable=Constants.TEXT_AMOUNT)
+
+            # tworzenie przycisków
+            self.close_button = Button(self.window,
+                                       text=Constants.TEXT_CLOSE_BUTTON,
+                                       cursor="hand2",
+                                       command=lambda: self.exit_platform(),
+                                       padx=10)
+            self.deposit_amount_button = Button(self.window,
+                                                text=Constants.TEXT_DEPOSIT_BUTTON,
+                                                cursor="hand2",
+                                                command=lambda: self.execute_transfer(Constants.STATE_DEPOSIT))
+            self.withdraw_amount_button = Button(self.window,
+                                                 text=Constants.TEXT_WITHDRAW_BUTTON,
+                                                 cursor="hand2",
+                                                 command=lambda: self.execute_transfer(Constants.STATE_WITHDRAWAL))
+            self.withdraw_all_funds_button = Button(self.window,
+                                                    text=Constants.TEXT_WITHDRAW_ALL_BUTTON,
+                                                    cursor="hand2",
+                                                    command=lambda: self.execute_transfer(
+                                                        Constants.STATE_WITHDRAWAL_ALL))
+
             self.show_widgets()  # wyświetlenie startowych widżetów
 
         Widgets.count += 1
-
-    def create_widgets(self):
-        """Metoda tworzy widżety takie jak przyciski, pola tekstowe, napisy"""
-
-        # tworzenie etykiet
-        self.main_title_label = Label(self.window,
-                                      text=Constants.TEXT_MAIN_TITLE,
-                                      bg=Constants.COLOUR_BACKGROUND,
-                                      fg=Constants.COLOUR_TEXT,
-                                      font=(
-                                          Constants.FONT_TYPEFACE,
-                                          Constants.FONT_SIZE_TITLE,
-                                          Constants.FONT_WEIGHT_TITLE),
-                                      pady=20)
-        self.main_description_label = Label(self.window,
-                                            text=Constants.TEXT_MAIN_DESCRIPTION,
-                                            bg=Constants.COLOUR_BACKGROUND,
-                                            fg=Constants.COLOUR_TEXT,
-                                            font=(Constants.FONT_TYPEFACE,
-                                                  Constants.FONT_SIZE_DESCRIPTION),
-                                            pady=20)
-        self.amount_label = Label(self.window,
-                                  text=Constants.TEXT_AMOUNT,
-                                  bg=Constants.COLOUR_BACKGROUND,
-                                  fg=Constants.COLOUR_TEXT,
-                                  padx=20,
-                                  font=(Constants.FONT_TYPEFACE,
-                                        Constants.FONT_SIZE_REGULAR))
-        self.account_balance_label_text = StringVar()
-        self.account_balance_label_text.set(self.get_current_account_balance_text())
-        self.account_balance_label = Label(self.window,
-                                           textvariable=self.account_balance_label_text,
-                                           padx=20,
-                                           bg=Constants.COLOUR_BACKGROUND,
-                                           fg=Constants.COLOUR_TEXT,
-                                           font=(Constants.FONT_TYPEFACE,
-                                                 Constants.FONT_SIZE_REGULAR))
-
-        # tworzenie pól tekstowych
-        self.amount_text = StringVar()
-        self.amount_entry = Entry(self.window, textvariable=Constants.TEXT_AMOUNT)
-
-        # tworzenie przycisków
-        self.close_button = Button(self.window,
-                                   text=Constants.TEXT_CLOSE_BUTTON,
-                                   cursor="hand2",
-                                   command=lambda: self.exit_platform(),
-                                   padx=10)
-        self.deposit_amount_button = Button(self.window,
-                                            text=Constants.TEXT_DEPOSIT_BUTTON,
-                                            cursor="hand2",
-                                            command=lambda: self.execute_transfer(Constants.STATE_DEPOSIT))
-        self.withdraw_amount_button = Button(self.window,
-                                             text=Constants.TEXT_WITHDRAW_BUTTON,
-                                             cursor="hand2",
-                                             command=lambda: self.execute_transfer(Constants.STATE_WITHDRAWAL))
-        self.withdraw_all_funds_button = Button(self.window,
-                                                text=Constants.TEXT_WITHDRAW_ALL_BUTTON,
-                                                cursor="hand2",
-                                                command=lambda: self.execute_transfer(Constants.STATE_WITHDRAWAL_ALL))
 
     def show_widgets(self):
         """Metoda wyświetla na ekranie zdefiniowane widżety"""
@@ -185,14 +219,6 @@ class Widgets:
 
         Transfer(self, self.window, state)
 
-    def get_current_account_balance_text(self):
-        return Constants.TEXT_CURRENT_BALANCE + str(Platform.account_balance) + Constants.TEXT_CURRENCY
-
-    def clear_entry_text(self, entry):
-        """Metoda czyści zawartość pola tekstowego"""
-
-        entry.delete(0, END)
-
     def exit_platform(self):
         """Metoda zamyka główne okno aplikacji."""
 
@@ -201,7 +227,7 @@ class Widgets:
             self.window.destroy()
 
 
-class Transfer(Widgets, Auxiliary):
+class Transfer(Widgets, Auxiliary, Account):
     """Obsługa transakcji wpłaty i wypłaty środków oraz aktualizacja stanu środków na kocie."""
 
     # TODO: clear textbox after successful transfer
@@ -220,7 +246,6 @@ class Transfer(Widgets, Auxiliary):
         else:
             amount = self.get_amount()
             is_correct = self.verify(amount, state)
-
             if is_correct:
                 amount = round(float(amount), 2)  # kwota jest poprawna, zaokrąglamy ją do dwóch miejsc po przecinku
                 if self.state == Constants.STATE_DEPOSIT:
@@ -235,8 +260,8 @@ class Transfer(Widgets, Auxiliary):
 
         response = messagebox.askokcancel("Potwierdź wpłatę", 'Czy na pewno chcesz wpłacić {} zł?'.format(amount))
         if response == 1:  # użytkownik potwierdził chęć wpłaty na konto
-            Platform.account_balance += amount
-            Platform.account_balance = round(Platform.account_balance, 2)
+            self.increase_account_balance(amount)
+
             messagebox.showinfo('', 'Pomyślnie dokonano wpłaty {} zł'.format(amount))
             self.update_label(self.widget_object.account_balance_label_text,
                               self.get_current_account_balance_text())
@@ -247,8 +272,7 @@ class Transfer(Widgets, Auxiliary):
 
         response = messagebox.askokcancel("Potwierdź wypłatę", 'Czy na pewno chcesz wypłacić {} zł?'.format(amount))
         if response == 1:  # użytkownik potwierdził chęć wypłaty na konto
-            Platform.account_balance -= amount
-            Platform.account_balance = round(Platform.account_balance, 2)
+            self.decrease_account_balance(amount)
             messagebox.showinfo('', 'Pomyślnie dokonano wypłaty {} zł'.format(amount))
             self.update_label(self.widget_object.account_balance_label_text,
                               self.get_current_account_balance_text())
@@ -257,14 +281,16 @@ class Transfer(Widgets, Auxiliary):
     def withdraw_all(self):
         """Metoda odpowiedzialna za wypłatę wszystkich środków z konta"""
 
-        if Platform.account_balance == 0:
+        current_account_balance = self.get_account_balance()
+        if current_account_balance == 0:
             return
         else:
             response = messagebox.askokcancel("Potwierdź wypłatę wszystkich środków",
-                                              'Czy na pewno chcesz wypłacić {} zł?'.format(Platform.account_balance))
+                                              'Czy na pewno chcesz wypłacić {} zł?'.format(current_account_balance))
             if response == 1:  # użytkownik potwierdził chęć wypłaty wszystkich środków
-                withdrawal_amount = Platform.account_balance
-                Platform.account_balance = 0
+                withdrawal_amount = current_account_balance
+                self.set_account_balance(0)
+
                 messagebox.showinfo('', 'Pomyślnie dokonano wypłaty {} zł'.format(withdrawal_amount))
                 self.update_label(self.widget_object.account_balance_label_text,
                                   self.get_current_account_balance_text())
@@ -294,7 +320,7 @@ class Transfer(Widgets, Auxiliary):
             return False
 
         if state == Constants.STATE_WITHDRAWAL:
-            if Platform.account_balance - amount < 0:
+            if self.get_account_balance() - amount < 0:
                 self.show_error(Constants.MESSAGE_ERROR_NEGATIVE_BALANCE)
                 return False
 

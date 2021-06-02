318985ed93b6004d75ccec88e25b2952e9be0696
Marcin Scieszka <marcin.scieszka@student.pk.edu.pl>
2021-06-02 14:34:10 +0200

    Udokumentowanie, segregacja stałych w klasie Constants

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,7 @@
 class Constants:
     """Klasa zawierająca stałe, które zostały wykorzystane podczas implementacji programu"""
 
+    # stałe odnoszące się do nazw pól tekstowych
     TEXT_MAIN_TITLE = 'Platforma transakcyjna Future Station'
     TEXT_MAIN_DESCRIPTION = 'Biuro maklerskie William-Scott Trade'
     TEXT_CLOSE_BUTTON = 'Wyjdź'
@@ -11,29 +12,34 @@ class Constants:
     TEXT_AMOUNT = 'Kwota'
     TEXT_CURRENT_BALANCE = 'Saldo: '
     # TEXT_ACCOUNT_VALUE = 'Wartość konta: '
-    TEXT_CURRENCY = ' zł'
+    TEXT_CURRENCY = ' zł'  # waluta, w której obsługiwane są transakcje
 
+    # stałe odnoszące się do zastosowanej palety kolorów
     COLOUR_BACKGROUND = '#2A2A2E'
     COLOUR_TEXT = '#FAFAFA'
 
+    # stałe odnoszące się do właściwości tekstu
     FONT_TYPEFACE = 'Ubuntu'
     FONT_WEIGHT_TITLE = 'bold'
     FONT_SIZE_TITLE = 20
     FONT_SIZE_DESCRIPTION = 14
     FONT_SIZE_REGULAR = 12
 
+    # stałe odnoszące się do treści komunikatów
     MESSAGE_ERROR = 'Błąd'
     MESSAGE_ERROR_VALUE = 'Podano nieprawidłową wartość'
     MESSAGE_ERROR_NEGATIVE_BALANCE = 'Niewystarczająca ilość środków do wypłaty'
+    MESSAGE_CONFIRM_EXIT = 'Potwierdź wyjście'
+    MESSAGE_CONFIRM_EXIT_TEXT = 'Czy na pewno chcesz opuścić platformę?'
 
+    # stałe odnoszące się do statusu transakcji gotówkowych wybranego przez użytkownika
     STATE_DEPOSIT = 0  # chęć wpłaty
     STATE_WITHDRAWAL = 1  # chęć wypłaty
     STATE_WITHDRAWAL_ALL = 2  # chęć wypłaty wszystkich wolnych środków
 
-    MESSAGE_CONFIRM_EXIT = 'Potwierdź wyjście'
-    MESSAGE_CONFIRM_EXIT_TEXT = 'Czy na pewno chcesz opuścić platformę?'
-
+    # separator danych stosowany podczas tworzenia listy firm
     DATA_SEPARATOR = ','
 
+    # stałe odnoszące się do typów zleceń akcji
     BUY_ORDER = 'Zlecenie zakupu akcji'
     SELL_ORDER = 'Zlecenie sprzedaży akcji'

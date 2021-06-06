bf8de57584e83430cc24b7aca2a67cd3273e364e
Marcin Scieszka <marcin.scieszka@student.pk.edu.pl>
2021-06-07 01:41:25 +0200

    Implementacja testu sprawdzającego poprawność sprzedaży uprzednio zakupionych akcji

--- src/Tests/test_sell_shares.py
+++ src/Tests/test_sell_shares.py
@@ -12,6 +12,7 @@ class TestSellShares(unittest.TestCase):
         cls.new_order = NewOrder()
         cls.company = DataProvider.get_company(5)
         cls.company_symbol = cls.company.get_symbol()
+        cls.company_price = cls.company.get_price()
 
     def test_sell_more_shares_than_owned_should_raise_exception(self):
         """Test sprawdza próbę sprzedaży większej liczby akcji, niż aktualnie posiadana przez użytkownika.
@@ -35,7 +36,7 @@ class TestSellShares(unittest.TestCase):
         account_balance_before_transaction = 5000
         self.account.set_account_balance(account_balance_before_transaction)
         nr_of_shares_to_sell = 10
-        transaction_value = nr_of_shares_to_sell * self.company.get_price()
+        transaction_value = nr_of_shares_to_sell * self.company_price
 
         # when
         self.new_order.handle_stock_sell_order(self.company, transaction_value, nr_of_shares_to_sell)
@@ -45,8 +46,33 @@ class TestSellShares(unittest.TestCase):
         self.assertEqual(nr_of_owned_shares_before_transaction, self.account.bought_companies[self.company_symbol])
 
     def test_sell_shares_should_decrease_nr_of_shares_owned_and_increase_account_balance(self):
-        """Test sprawdza poprawność sprzedaży uprzednio zakupionych akcji
-        Oczekiwania: odpowiednie zmniejszenie liczby posiadanych akcji oraz zwiększenie wartości wolnych środków """
+        """Test sprawdza poprawność sprzedaży uprzednio zakupionych akcji.
+        Oczekiwania: odpowiednie zmniejszenie liczby posiadanych akcji, zwiększenie wartości wolnych środków,
+        zmniejszenie wartości posiadanych akcji,
+        wartość konta nie powinna się zmienić"""
+
+        # given
+        self.account.set_account_balance(0)
+        account_balance_before_transaction = self.account.get_account_balance()
+        self.account.set_value_of_shares_held(5000.0)
+        value_of_owned_shares_before_transaction = self.account.get_value_of_shares_held()
+        self.account.bought_companies[self.company_symbol] = 8
+        nr_of_company_shares_owned_before_transaction = self.account.bought_companies[self.company_symbol]
+        nr_of_shares_to_sell = 5
+        transaction_value = nr_of_shares_to_sell * self.company_price
+        expected_nr_of_shares_owned_after_transaction = nr_of_company_shares_owned_before_transaction - nr_of_shares_to_sell
+        expected_account_balance = account_balance_before_transaction + transaction_value
+        expected_value_of_owned_shares = value_of_owned_shares_before_transaction - transaction_value
+        expected_total_account_value = self.account.get_total_account_value_text()
+
+        # when
+        self.new_order.handle_stock_sell_order(self.company, transaction_value, nr_of_shares_to_sell)
+
+        # then
+        self.assertEqual(expected_nr_of_shares_owned_after_transaction, self.account.bought_companies[self.company_symbol])
+        self.assertEqual(expected_account_balance, self.account.get_account_balance())
+        self.assertEqual(expected_value_of_owned_shares, self.account.get_value_of_shares_held())
+        self.assertEqual(expected_total_account_value, self.account.get_total_account_value_text())
 
 
 if __name__ == '__main__':

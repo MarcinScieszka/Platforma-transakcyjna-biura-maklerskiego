490e53e104ac84ed71523fb3088a3e2fb75a57e1
MarcinScieszka <79609951+MarcinScieszka@users.noreply.github.com>
2022-03-04 11:34:29 +0100

    simplify main

--- src/main.py
+++ src/main.py
@@ -1,18 +1,12 @@
 import tkinter as tk
-
 from src.gui import Gui
 
 
-class Main:
-    """Główna klasa programu.
-    Wywołanie klasy Gui odpowiedzialnej za interfejs graficzny"""
-
-    @staticmethod
-    def main():
-        window = tk.Tk()
-        Gui(window)
-        window.mainloop()
+def main():
+    window = tk.Tk()
+    Gui(window)
+    window.mainloop()
 
 
 if __name__ == '__main__':
-    Main.main()
+    main()
